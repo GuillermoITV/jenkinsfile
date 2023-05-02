d9782b700b9cbf94ea0992ae95f5037140f81c1b
GuillermoITV <31926950+GuillermoITV@users.noreply.github.com>
2023-05-02 13:24:59 -0600

    Create mi_script.sh

---
+++ mi_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Comprobamos que se haya pasado un argumento (la ruta a liberar espacio)
+if [ $# -eq 0 ]; then
+    echo "Debe proporcionar una ruta a la que liberar espacio."
+    exit 1
+fi
+
+# Obtenemos el uso actual del disco en la ruta especificada
+echo "Espacio usado antes de la limpieza:"
+df -h $1
+
+# Eliminamos archivos temporales y cachés
+echo "Eliminando archivos temporales y cachés..."
+find $1 -type f \( -name "*.tmp" -o -name "*.log" -o -name "*.cache" \) -delete
+
+# Vaciamos la papelera de reciclaje
+echo "Vaciando la papelera de reciclaje..."
+rm -rf $HOME/.local/share/Trash/*
+
+# Obtenemos el uso actual del disco en la ruta especificada después de la limpieza
+echo "Espacio usado después de la limpieza:"
+df -h $1
